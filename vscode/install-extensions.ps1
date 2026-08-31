93ed0870d7bff398fd261eeb351933cc1a58bcd8
Raphael Vallée <72668371+Tranzitron@users.noreply.github.com>
2026-08-30 22:40:19 -0400

    VSCode - Install Extensions

--- vscode/install-extensions.ps1
+++ vscode/install-extensions.ps1
@@ -1,6 +1,15 @@
-﻿Get-Content -Path "$PSScriptRoot\extensions.txt" | ForEach-Object {
-    $extension = $_.Trim()
-    if ($extension -and -not $extension.StartsWith("#")) {
-        code --install-extension $extension
-    }
+﻿$targetExtensions = Get-Content -Path "$PSScriptRoot\extensions.txt" | 
+ForEach-Object { $_.Trim() } | 
+Where-Object { $_ -and -not $_.StartsWith("#") }
+
+$installedExtensions = code --list-extensions
+
+# Uninstall extensions not present in the file
+$installedExtensions | Where-Object { $targetExtensions -notcontains $_ } | ForEach-Object {
+    code --uninstall-extension $_
+}
+
+# Install missing extensions from the file
+$targetExtensions | Where-Object { $installedExtensions -notcontains $_ } | ForEach-Object {
+    code --install-extension $_
 }
